68d59539a9844a00833cd107f5f2df6f9b73b9ec
Leonardo Arencibia <leonardoarencibia2002@gmail.com>
2024-12-11 20:35:51 -0500

    feat: Add YouTube Videos in Bottom Section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
