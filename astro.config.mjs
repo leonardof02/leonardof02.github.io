606fe0b0c888e25edf06f4dbffd07a495d28353f
Leonardo Arencibia <leonardoarencibia2002@gmail.com>
2024-12-12 06:29:18 -0500

    fix: Remove SSR for Youtube API error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercelAdapter from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercelAdapter(),
 });
