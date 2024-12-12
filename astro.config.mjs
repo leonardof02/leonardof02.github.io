1287ae44d948c9524ab50d263dacc7de85f2437f
Leonardo Arencibia <leonardoarencibia2002@gmail.com>
2024-12-12 06:24:29 -0500

    fix: Add Vercel Adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercelAdapter from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
+  adapter: vercelAdapter(),
 });
